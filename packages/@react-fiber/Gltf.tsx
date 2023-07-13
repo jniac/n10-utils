66a7a9645f151816705479169656001c0d555d89
jniac <joseph.merdrignac@gmail.com>
2023-07-13 23:03:55 +0200

    gltf loader / component

---
+++ packages/@react-fiber/Gltf.tsx
@@ -0,0 +1,75 @@
+import { Group, Mesh, Texture } from 'three'
+import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GroupProps } from '@react-three/fiber'
+import { useEffects } from '../react/hooks'
+import { lazy } from '../../lazy'
+
+const local = lazy(() => {
+  const loader = new GLTFLoader()
+  const map = new Map<string, { gltf: GLTF, count: number }>()
+  const register = (url: string, gltf: GLTF) => {
+    const bundle = map.get(url)
+    if (bundle) {
+      bundle.count++ 
+    } else {
+      const bundle = {
+        gltf,
+        count: 1,
+      }
+      map.set(url, bundle)
+    }
+  }
+  const free = (url: string) => {
+    const bundle = map.get(url)
+    if (bundle) {
+      bundle.count--
+      if (bundle.count === 0) {
+        bundle.gltf.scene.traverse(child => {
+          if (child instanceof Mesh) {
+            for (const value of Object.values(child.material)) {
+              if (value instanceof Texture) {
+                value.dispose()
+              }                
+            }
+            child.geometry.dispose()
+            child.material.dispose()
+          }
+        })
+      }
+    }      
+  }
+  return {
+    loader,
+    register,
+    free,
+  }
+})
+
+type Props = {
+  url: string
+} & GroupProps
+
+/**
+ * Load a gltf / glb file, and automatically dispose the resources (geometry, 
+ * material & textures) when the componenent is destroyed.
+ */
+export function Gltf({
+  url,
+  ...props
+}: Props) {
+  const { ref } = useEffects<Group>(function* (group) {
+    local().loader.load(url, gltf => {
+      local().register(url, gltf)
+      group.add(gltf.scene)
+    })
+    yield () => {
+      local().free(url)
+    }
+  }, [])
+  return (
+    <group 
+      ref={ref}
+      {...props}
+    />
+  )
+}
