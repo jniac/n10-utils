cfff69c4d4fb77bc28e7e4dfa797c36232c902ac
jniac <joseph.merdrignac@gmail.com>
2023-07-13 23:46:04 +0200

    tap info (clientX, clientY etc.)

--- dom/handle/tap.ts
+++ dom/handle/tap.ts
@@ -8,7 +8,10 @@ const defaultParams = {
 }
 
 type Info = {
+  timestamp: number
   downTarget: HTMLElement
+  clientX: number
+  clientY: number
 }
 
 type Params = Partial<typeof defaultParams & {
@@ -22,27 +25,25 @@ function handleTap(element: HTMLElement, params: Params): DestroyableObject {
     onTap,
   } = { ...defaultParams, ...params }
 
-  const downState = {
+  const info: Info = {
     timestamp: -1,
+    downTarget: null!,
     clientX: 0,
     clientY: 0,
   }
-  const info: Info = {
-    downTarget: null!,
-  }
 
   const onPointerDown = (event: PointerEvent) => {
-    downState.timestamp = Date.now()
-    downState.clientX = event.clientX
-    downState.clientY = event.clientY
+    info.timestamp = Date.now()
+    info.clientX = event.clientX
+    info.clientY = event.clientY
     window.addEventListener("pointerup", onPointerUp)
     info.downTarget = event.target as HTMLElement
   }
   const onPointerUp = (event: PointerEvent) => {
     window.removeEventListener("pointerup", onPointerUp)
-    const duration = (Date.now() - downState.timestamp) / 1e3
-    const x = event.clientX - downState.clientX
-    const y = event.clientY - downState.clientY
+    const duration = (Date.now() - info.timestamp) / 1e3
+    const x = event.clientX - info.clientX
+    const y = event.clientY - info.clientY
     const distance = Math.sqrt(x * x + y * y)
     if (distance <= maxDistance && duration < maxDuration) {
       onTap?.(info)
