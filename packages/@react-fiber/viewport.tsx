9dd72f3e23580a1fd78923281bca5729bba927fb
jniac <joseph.merdrignac@gmail.com>
2023-07-18 18:02:30 +0200

    viewport has its own camera, scene & extraScene (for gizmo rendering)

--- packages/@react-fiber/viewport.tsx
+++ packages/@react-fiber/viewport.tsx
@@ -1,39 +1,75 @@
 import { PropsWithChildren, createContext, useContext, useMemo } from 'react'
-import { Vector2 } from 'three'
+import { Camera, Object3D, PerspectiveCamera, Vector2 } from 'three'
 import { Canvas, CanvasProps, useThree } from '@react-three/fiber'
 
 import { DestroyableObject } from '../../types'
-import { useEffects } from '../react/hooks'
+import { digestProps, useEffects } from '../react/hooks'
 import { windowClock } from '../../clock'
-import { digest } from '../../digest'
+import { VertigoCamera } from '../three/vertigo/VertigoCamera'
 
 const defaultViewportProps = {
+  main: false,
   box: [0, 0, 1, 1] as [x: number, y: number, width: number, height: number],
   zIndex: 0,
 }
 
-type ViewportProps = typeof defaultViewportProps
+type ViewportProps = Partial<typeof defaultViewportProps & {
+  onBeforeRender: (info: { mainCamera: Camera, camera: Camera, viewport: Viewport, mainViewport: Viewport }) => void
+  camera: Camera
+  scene: Object3D
+  extraScene: Object3D
+}>
+
+class Viewport {
+  props: ViewportProps
+  x = 0
+  y = 0
+  width = 0
+  height = 0
+  isMainViewport = false
+  constructor(props: ViewportProps) {
+    this.props = props
+  }
+  getAspect(): number {
+    return this.width / this.height
+  }
+}
 
 class ViewportManager {
-  private _viewports: Set<ViewportProps> = new Set()
-  private _sortedViewports: ViewportProps[] = []
+  private static _defaultMainViewport = new Viewport(defaultViewportProps)
+  private _viewports: Set<Viewport> = new Set()
+  private _sortedViewports: Viewport[] = []
+  private _mainViewport: Viewport = ViewportManager._defaultMainViewport
   private _dirty = false
-  private _sort() {
+  private _update() {
     this._sortedViewports = [...this._viewports]
-      .sort((A, B) => (A.zIndex ?? 0) - (B.zIndex ?? 0))
+      .sort((A, B) => (A.props.zIndex ?? 0) - (B.props.zIndex ?? 0))
+
+    const mainViewport = this._sortedViewports.find(v => v.props.main) 
+      ?? this._sortedViewports[0] 
+      ?? ViewportManager._defaultMainViewport
+    for (const viewport of this._sortedViewports) {
+      viewport.isMainViewport = viewport === mainViewport
+    }
+    this._mainViewport = mainViewport
+
     this._dirty = false
   }
+  getMainViewport(): Viewport {
+    return this._mainViewport
+  }
   addViewport(props: ViewportProps): DestroyableObject {
-    this._viewports.add(props)
+    const viewport = new Viewport(props)
+    this._viewports.add(viewport)
     this._dirty = true
     const destroy = () => {
-      this._viewports.delete(props)
+      this._viewports.delete(viewport)
     }
     return { destroy }
   }
   *iterateViewports() {
     if (this._dirty) {
-      this._sort()
+      this._update()
     }
     yield* this._sortedViewports
   }
@@ -44,32 +80,68 @@ const ViewportContext = createContext<ViewportManager>(null!)
 function ViewportProvider({ 
   tickOrder, 
   children, 
-}: PropsWithChildren<{ tickOrder: number }>) {
+}: PropsWithChildren<{ 
+  tickOrder: number
+}>) {
   const manager = useMemo(() => {
     return new ViewportManager()
   }, [])
-  const { gl, camera, scene } = useThree()
+
+  const { 
+    gl,
+    camera: mainCamera,
+    scene: mainScene,
+  } = useThree()
+
   useEffects(function* () {
+    gl.autoClear = false
+    
     const size = new Vector2()
     yield windowClock().onTick(tickOrder, () => {
+      gl.clear(true, true, true)
       gl.resetState()
       gl.getSize(size)
       
+      const mainViewport = manager.getMainViewport()
       for (const viewport of manager.iterateViewports()) {
         const {
+          scene = mainScene,
+          extraScene,
+          camera = mainCamera,
           box = defaultViewportProps.box,
-        } = viewport
+          onBeforeRender,
+        } = viewport.props
 
         let [x, y, width, height] = box
         x *= size.x
-        width *= size.x
         y *= size.y
+        width *= size.x
         height *= size.y
+
+        viewport.x = x
+        viewport.y = y
+        viewport.width = width
+        viewport.height = height
+
+        if (camera instanceof PerspectiveCamera) {
+          if (camera instanceof VertigoCamera) {
+            camera.updateVertigoCamera(viewport.getAspect())
+          } else {
+            camera.aspect = viewport.getAspect()
+            camera.updateProjectionMatrix()
+          }
+        }
+
+        onBeforeRender?.({ camera, mainCamera, viewport, mainViewport })
 
         gl.setViewport(x, y, width, height)
         gl.setScissor(x, y, width, height)
         gl.setScissorTest(true)
         gl.render(scene, camera)
+        
+        if (extraScene) {
+          gl.render(extraScene, camera)
+        }
       }
     })
   }, [tickOrder])
@@ -80,11 +152,11 @@ function ViewportProvider({
   )
 }
 
-function Viewport(props: Partial<ViewportProps>) {
+function ViewportComponent(props: ViewportProps) {
   const manager = useContext(ViewportContext)
   useEffects(function* () {
     yield manager.addViewport({ ...defaultViewportProps, ...props })
-  }, [digest(props)])
+  }, [digestProps(props)])
   return (
     null
   )
@@ -106,7 +178,11 @@ function ViewportCanvas({
   )
 }
 
+export type {
+  ViewportProps,
+}
+
 export {
-  Viewport,
+  ViewportComponent as Viewport,
   ViewportCanvas,
 }
