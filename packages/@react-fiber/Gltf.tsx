aa116415c49f3c44084995b80ae578d3de0c852e
jniac <joseph.merdrignac@gmail.com>
2023-07-31 23:23:30 +0200

    detail

--- packages/@react-fiber/Gltf.tsx
+++ packages/@react-fiber/Gltf.tsx
@@ -63,6 +63,7 @@ export function Gltf({
   ...props
 }: Props) {
   const { ref } = useEffects<Group>(function* (group) {
+    group.name = Gltf.name
     local().loader.load(url, gltf => {
       local().register(url, gltf)
       if (pivotShift) {
