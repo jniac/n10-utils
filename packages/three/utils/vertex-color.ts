14659ddaccc3070503e9096f3943141268fa825c
jniac <joseph.merdrignac@gmail.com>
2023-08-04 11:07:14 +0200

    create or update the "color" buffer

--- packages/three/utils/vertex-color.ts
+++ packages/three/utils/vertex-color.ts
@@ -1,10 +1,27 @@
-import { BufferAttribute, BufferGeometry, Color } from 'three'
+import { clamp } from '@/n10-utils/math/basics'
+import { BufferAttribute, BufferGeometry, Color, ColorRepresentation } from 'three'
 
-export const setVertexColor = (geometry: BufferGeometry, { r, g, b }: Color) => {
+export const setVertexColor = (geometry: BufferGeometry, color: ColorRepresentation, range: [startIndex: number, endIndex: number] = [0, -1]) => {
   const count = geometry.attributes.position.count
-  const color = new BufferAttribute(new Float32Array(count * 3), 3)
-  for (let i = 0; i < count; i++) {
-    color.setXYZ(i, r, g, b)
+
+  // Compute indexes
+  let [startIndex, endIndex] = range
+  if (startIndex < 0) {
+    startIndex += count + 1
   }
-  geometry.setAttribute('color', color)
+  if (endIndex < 0) {
+    endIndex += count + 1
+  }
+  startIndex = clamp(startIndex, 0, count)
+  endIndex = clamp(endIndex, 0, count)
+  if (endIndex < startIndex) {
+    endIndex = startIndex
+  }
+  
+  const colorAtt = geometry.attributes.color ?? new BufferAttribute(new Float32Array(count * 3), 3)
+  const { r, g, b } = new Color(color)
+  for (let i = startIndex; i < endIndex; i++) {
+    colorAtt.setXYZ(i, r, g, b)
+  }
+  geometry.setAttribute('color', colorAtt)
 }
