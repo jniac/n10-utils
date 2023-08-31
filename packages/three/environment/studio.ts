ee42808310f27e9c261f06712caf95a67f40b5a8
jniac <joseph.merdrignac@gmail.com>
2023-08-31 14:09:46 +0200

    studio is better (intensity option etc)

--- packages/three/environment/studio.ts
+++ packages/three/environment/studio.ts
@@ -1,20 +1,47 @@
 import { BackSide, BoxGeometry, CircleGeometry, DoubleSide, Mesh, MeshLambertMaterial, PMREMGenerator, PointLight, Scene, Texture, WebGLRenderer } from 'three'
+import { PRNG } from '../../../math/random'
+import { lerp } from '../../../math/basics'
 import { ShaderForge } from '../shader-forge'
 import { glsl_easings } from '../../../glsl/easings'
-import { PRNG } from '../../../math/random'
-import { lerp } from '@/n10-utils/math/basics'
-import { glsl_utils } from '@/n10-utils/glsl/utils'
+import { glsl_utils } from '../../../glsl/utils'
+
+function createBox({ lightIntensity = 1 } = {}) {
+  const geometry = new BoxGeometry(10, 10, 10)
+  const material = new MeshLambertMaterial({
+    side: BackSide,
+    color: '#bbb',
+  })
+  material.onBeforeCompile = shader => ShaderForge.with(shader)
+    .uniforms({
+      uLightIntensity: { value: lightIntensity },
+    })
+    .varying({
+      vPosition: 'vec3',
+    })
+    .vertex.mainAfterAll(/* glsl */`
+      vPosition = (position + 10.) / 20.;
+    `)
+    .fragment.top(
+      glsl_utils,
+      glsl_easings,
+    )
+    .fragment.after('map_fragment', /* glsl */`
+      diffuseColor.rgb *= uLightIntensity * easeInout3(vPosition.y) * 3.;
+    `)
+  return new Mesh(geometry, material)
+}
 
 function createMandarine({
-  emissiveIntensity = 10,
+  lightIntensity = 10,
   scale = 1,
   gradientForce = 2 as 1 | 2 | 3 | 4 | 5,
 } = {}) {
   const geometry = new CircleGeometry(1, 24).scale(scale, scale, scale)
   const material = new MeshLambertMaterial({
     transparent: true,
+    color: 'black',
     emissive: '#ffffff',
-    emissiveIntensity,
+    emissiveIntensity: lightIntensity,
     side: DoubleSide,
     depthWrite: false,
     depthTest: false,
@@ -31,32 +58,14 @@ function createMandarine({
   return new Mesh(geometry, material)
 }
 
-function createBox() {
-  const geometry = new BoxGeometry(10, 10, 10)
-  const material = new MeshLambertMaterial({
-    side: BackSide,
-    color: '#bbb',
-  })
-  material.onBeforeCompile = shader => ShaderForge.with(shader)
-    .varying({
-      vPosition: 'vec3',
-    })
-    .vertex.mainAfterAll(/* glsl */`
-      vPosition = (position + 10.) / 20.;
-    `)
-    .fragment.top(
-      glsl_utils,
-      glsl_easings,
-    )
-    .fragment.after('map_fragment', /* glsl */`
-      diffuseColor.rgb *= easeInout3(vPosition.y) * 2.;
-    `)
-  return new Mesh(geometry, material)
+const defaultSceneProps = {
+  lightIntensity: 1,
 }
 
-export function createStudioEnvironmentTexture(renderer: WebGLRenderer): Texture {
-  const pmremGenerator = new PMREMGenerator(renderer)
-  pmremGenerator.compileCubemapShader()
+function createScene(props: Partial<typeof defaultSceneProps> = {}): Scene {
+  const {
+    lightIntensity,
+  } = { ...defaultSceneProps, ...props }
 
   const scene = new Scene()
 
@@ -64,16 +73,20 @@ export function createStudioEnvironmentTexture(renderer: WebGLRenderer): Texture
   pointLight.position.set(0, 0, 1)
   scene.add(pointLight)
 
-  const box = createBox()
+  const box = createBox({
+    lightIntensity,
+  })
   scene.add(box)
 
-  const mandarine1 = createMandarine()
+  const mandarine1 = createMandarine({
+    lightIntensity,
+  })
   mandarine1.position.set(3, 3, 0)
   mandarine1.rotation.set(0, Math.PI / 2, 0)
   scene.add(mandarine1)
 
   const mandarine2 = createMandarine({
-    emissiveIntensity: 1,
+    lightIntensity,
     scale: 1.5,
   })
   mandarine2.position.set(-1, 2, 4)
@@ -86,7 +99,7 @@ export function createStudioEnvironmentTexture(renderer: WebGLRenderer): Texture
     const y = PRNG.random()
     const z = PRNG.random()
     const mandarine = createMandarine({
-      emissiveIntensity: PRNG.range(1, 4, x => x ** 4) * lerp(.33, 2, y),
+      lightIntensity: lightIntensity * PRNG.range(1, 4, x => x ** 4) * lerp(.33, 2, y),
       gradientForce: Math.round(PRNG.range(1, 5)) as any,
       scale: PRNG.random() * 1.25,
     })
@@ -95,6 +108,25 @@ export function createStudioEnvironmentTexture(renderer: WebGLRenderer): Texture
     scene.add(mandarine)
   }
 
+  return scene
+}
+
+type Props = Partial<typeof defaultSceneProps>
+
+function createStudioEnvTexture(renderer: WebGLRenderer, props: Props = {}): Texture {
+  const pmremGenerator = new PMREMGenerator(renderer)
+  pmremGenerator.compileCubemapShader()
+
+  const scene = createScene(props)
+
   const generatedCubeRenderTarget = pmremGenerator.fromScene(scene, 1)
   return generatedCubeRenderTarget.texture
+}
+
+export type {
+  Props as StudioEnvProps,
+}
+
+export {
+  createStudioEnvTexture,
 }
