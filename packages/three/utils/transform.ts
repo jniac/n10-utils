ce337c8cd4a9a645958cc1238825eccce34d6e0c
jniac <joseph.merdrignac@gmail.com>
2023-09-14 18:25:26 +0200

    resetTransform()

--- packages/three/utils/transform.ts
+++ packages/three/utils/transform.ts
@@ -1,5 +1,15 @@
 import { Object3D } from 'three'
 
+export function resetTransform(current: Object3D, { resetMatrices = true } = {}) {
+  current.position.set(0, 0, 0)
+  current.rotation.set(0, 0, 0)
+  current.scale.set(1, 1, 1)
+  if (resetMatrices) {
+    current.matrix.identity()
+    current.matrixWorld.identity()
+  }
+}
+
 export function copyTransform(current: Object3D, target: Object3D, { copyMatrices = true } = {}) {
   current.position.copy(target.position)
   current.rotation.copy(target.rotation)
