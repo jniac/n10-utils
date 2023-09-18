5ec8f4716d0b1a1ea10f8ff042989065e8ac8494
jniac <joseph.merdrignac@gmail.com>
2023-09-18 13:50:30 +0200

    handle keyboard rfctr: using an "info" object (future proof)

--- dom/handle/keyboard.ts
+++ dom/handle/keyboard.ts
@@ -1,12 +1,22 @@
 import { Destroyable, StringFilter } from '@/n10-utils/types'
 import { applyStringFilter } from '@/n10-utils/string'
 
-export function handleKeyboard(listeners: [filter: StringFilter, callback: (event: KeyboardEvent) => void][]): Destroyable {
+type Info = {
+  event: KeyboardEvent
+}
+
+type KeyboardListenerEntry = [
+  filter: StringFilter, 
+  callback: (info: Info) => void,
+]
+
+export function handleKeyboard(listeners: KeyboardListenerEntry[]): Destroyable {
   const onKeyDown = (event: KeyboardEvent): void => {
+    const info: Info = { event }
     for (let i = 0, max = listeners.length; i < max; i++) {
       const [filter, callback] = listeners[i]
       if (applyStringFilter(event.code, filter)) {
-        callback(event)
+        callback(info)
       }
     }
   }
