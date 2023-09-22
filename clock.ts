3f12de37729814ae2ea646dea67d3db6a0871277
jniac <joseph.merdrignac@gmail.com>
2023-09-22 09:40:53 +0200

    clock: "freeze", deltaTimeOld, windowDeltaTime etc.

--- clock.ts
+++ clock.ts
@@ -1,6 +1,6 @@
+import { DestroyableObject } from './types'
 import { inverseLerp } from './math/basics'
 import { easeInOut2 } from './math/easing'
-import { DestroyableObject } from './types'
 
 type ClockState = Readonly<{
 	timeScale: number
@@ -8,8 +8,10 @@ type ClockState = Readonly<{
 	timeOld: number
 	frame: number
 	deltaTime: number
+	deltaTimeOld: number
+	windowDeltaTime: number
 	maxDeltaTime: number
-	
+
 	updateDuration: number
 	updateFadeDuration: number
 	updateLastRequest: number
@@ -47,11 +49,36 @@ class Listeners {
 	}
 }
 
-class Clock {
-	listeners = new Listeners()
+
+let windowTime = 0
+const animationFrameCallbacks = new Set<(windowDeltaTime: number) => void>()
+
+if (typeof window !== 'undefined') {
+	windowTime = window.performance.now() / 1e3
+	const animationFrame = (ms: number) => {
+		window.requestAnimationFrame(animationFrame)
+		const windowTimeOld = windowTime
+		windowTime = ms / 1e3
+		const windowDeltaTime = windowTime - windowTimeOld
+		for (const callback of animationFrameCallbacks) {
+			callback(windowDeltaTime)
+		}
+	}
+	window.requestAnimationFrame(animationFrame)
+}
+
+class Clock implements DestroyableObject {
 	timeScale = 1
 	maxDeltaTime = .1
 	frame = 0
+	
+	destroy: () => void
+	
+	private _updateListeners = new Listeners()
+	private _freezeListeners = new Listeners()
+	private _unfreezeListeners = new Listeners()
+	private _freezed = false
+	private _requestAnimationFrame = false
 
 	private _state: ClockState = {
 		timeScale: 1,
@@ -60,72 +87,90 @@ class Clock {
 		frame: 0,
 		maxDeltaTime: .1,
 		deltaTime: 0,
+		deltaTimeOld: 0,
+		windowDeltaTime: 0,
 
 		updateDuration: 3,
 		updateFadeDuration: 1,
 		updateLastRequest: 0,
 	}
+
+	get freezed() { return this._freezed }
 	get state() { return this._state }
 
-	private _requestAnimationFrame = false
+	constructor(updateDuration?: number) {
+		if (updateDuration !== undefined) {
+			this._state = { ...this._state, updateDuration }
+		}
+		
+		const update = (windowDeltaTime: number) => {
+			// Auto-pause handling:
+			let { updateDuration, updateFadeDuration, updateLastRequest } = this._state
+			if (this._requestAnimationFrame) {
+				this._requestAnimationFrame = false
+				updateLastRequest = windowTime
+			}
+			const updateTime1 = updateLastRequest + updateDuration
+			const updateTime2 = updateTime1 + updateFadeDuration
+			const updateTimeScale = easeInOut2(1 - inverseLerp(updateTime1, updateTime2, windowTime))
 
-	constructor() {
-		if (typeof window !== 'undefined') {
-			let windowTime = window.performance.now() / 1e3
-			const update = (ms: number) => {
-				window.requestAnimationFrame(update)
-				const windowTimeOld = windowTime
-				windowTime = ms / 1e3
+			// Time handling:
+			const { timeScale, maxDeltaTime } = this
+			const deltaTimeOld = this._state.deltaTime
+			const deltaTime = Math.min(maxDeltaTime, windowDeltaTime) * timeScale * updateTimeScale
+			const timeOld = this._state.time
+			const time = timeOld + deltaTime
+			const state = Object.freeze({
+				timeScale,
+				deltaTimeOld,
+				deltaTime,
+				windowDeltaTime,
+				time,
+				timeOld,
+				maxDeltaTime,
+				frame: this.frame,
 
-				// Auto-pause handling:
-				let { updateDuration, updateFadeDuration, updateLastRequest } = this._state
-				if (this._requestAnimationFrame) {
-					this._requestAnimationFrame = false
-					updateLastRequest = windowTime
+				updateDuration,
+				updateFadeDuration,
+				updateLastRequest,
+			})
+			this._state = state
+
+			const freezed = 
+				deltaTime === 0 // Current delta time should be zero.
+				&& deltaTimeOld === 0 // But the old delta time also (this allows one "zero-delta-time" callback).
+				&& time === timeOld // Time should not have been update by any other ways.
+			if (freezed === false) {
+				if (this._freezed) {
+					this._unfreezeListeners.call(state)
 				}
-				const updateTime1 = updateLastRequest + updateDuration
-				const updateTime2 = updateTime1 + updateFadeDuration
-				const updateTimeScale = easeInOut2(1 - inverseLerp(updateTime1, updateTime2, windowTime))
-
-				// Time handling:
-				const { timeScale, maxDeltaTime } = this
-				const deltaTime = Math.min(maxDeltaTime, windowTime - windowTimeOld) * timeScale * updateTimeScale
-				const timeOld = this._state.time
-				const time = timeOld + deltaTime
-				const state = Object.freeze({
-					timeScale,
-					deltaTime,
-					time,
-					timeOld,
-					maxDeltaTime,
-					frame: this.frame,
-
-					updateDuration,
-					updateFadeDuration,
-					updateLastRequest,
-				})
-				this._state = state
-
-				const freezed = deltaTime === 0 && time === timeOld
-				if (freezed === false) {
-					this.listeners.call(state)
-					this.frame++
+				this._updateListeners.call(state)
+				this.frame++
+			} else {
+				if (this._freezed === false) {
+					this._freezeListeners.call(state)
 				}
 			}
-			window.requestAnimationFrame(update)
+
+			this._freezed = freezed
+		}
+
+		animationFrameCallbacks.add(update)
+		this.destroy = () => {
+			animationFrameCallbacks.delete(update)
 		}
 	}
 
 	onTick(callback: (clock: ClockState) => void): DestroyableObject
 	onTick(order: number, callback: (clock: ClockState) => void): DestroyableObject
-	onTick(...args: any[]): DestroyableObject{
+	onTick(...args: any[]): DestroyableObject {
 		if (args.length === 1) {
 			return this.onTick(0, args[0])
 		}
 		const [order, callback] = args as [number, (clock: ClockState) => void]
-		this.listeners.add(order, callback)
+		this._updateListeners.add(order, callback)
 		const destroy = () => {
-			this.listeners.remove(callback)
+			this._updateListeners.remove(callback)
 		}
 		return { destroy }
 	}
@@ -144,7 +189,7 @@ class Clock {
 			updateDuration: Math.max(0, value),
 		})
 	}
-	
+
 	/**
 	 * Returns a promise that will be resolved at the next frame. The promise's 
 	 * inner value is the current frame.
@@ -153,10 +198,10 @@ class Clock {
 		this.requestUpdate()
 		return new Promise<number>(resolve => {
 			const callback = () => {
-				this.listeners.remove(callback)
+				this._updateListeners.remove(callback)
 				resolve(this.frame)
 			}
-			this.listeners.add(0, callback)
+			this._updateListeners.add(0, callback)
 		})
 	}
 
@@ -175,11 +220,11 @@ class Clock {
 				count++
 				this.requestUpdate()
 				if (count === frameCount) {
-					this.listeners.remove(callback)
+					this._updateListeners.remove(callback)
 					resolve(this.frame)
 				}
 			}
-			this.listeners.add(0, callback)
+			this._updateListeners.add(0, callback)
 		})
 	}
 }
