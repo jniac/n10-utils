f0e6e35819990a81445d3eb31bfae0f040f60290
jniac <joseph.merdrignac@gmail.com>
2024-03-21 01:25:51 +0100

    psine

--- math/misc.ts
+++ math/misc.ts
@@ -6,3 +6,14 @@ export const pcurve = (x: number, a: number, b: number): number => {
   const k = Math.pow(a + b, a + b) / (Math.pow(a, a) * Math.pow(b, b))
   return k * Math.pow(x, a) * Math.pow(1.0 - x, b)
 }
+
+/**
+ * Power Sine
+ * https://jniac.github.io/some-curves/curves/psine/
+ */
+export const psine = (x: number, p = 3) => {
+  const s = Math.sin(x)
+  const sign = s < 0 ? -1 : 1
+  const r = 1 - Math.pow(1 - sign * s, p)
+  return r * sign
+}
