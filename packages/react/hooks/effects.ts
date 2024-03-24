78281f915ff72f7dc45d44024f82f0363622f8a7
jniac <joseph.merdrignac@gmail.com>
2024-03-24 23:22:05 +0100

    useEffects overloads: allowing the options to be passed first

--- packages/react/hooks/effects.ts
+++ packages/react/hooks/effects.ts
@@ -2,9 +2,13 @@ import { DependencyList, MutableRefObject, useEffect, useLayoutEffect, useMemo, 
 import { Destroyable } from '../../../types'
 import { digestProps } from './digestProps'
 
+type UseEffectsCallback<T> =
+	(value: T) => void | Generator<void | Destroyable, void, unknown>
+
 type UseEffectsReturn<T> = {
 	ref: MutableRefObject<T>
 }
+
 type UseEffectsOptions = Partial<{
 	moment: 'effect' | 'layoutEffect' | 'memo'
 	/**
@@ -15,18 +19,37 @@ type UseEffectsOptions = Partial<{
 	useDigestProps: boolean
 }>
 
-export function useEffects<T = undefined>(
-	callback: (value: T) => void | Generator<void | Destroyable, void, unknown>,
+function parseArgs<T>(args: any[]): [UseEffectsCallback<T>, DependencyList | 'always', UseEffectsOptions] {
+	const [arg0, arg1, arg2] = args
+	if (typeof arg0 === 'object') {
+		return [arg1, arg2, arg0]
+	}
+	return [arg0, arg1, arg2 ?? {}]
+}
+
+function useEffects<T = undefined>(
+	options: UseEffectsOptions,
+	callback: UseEffectsCallback<T>,
 	deps: DependencyList | 'always',
-	{
+): UseEffectsReturn<T>
+
+function useEffects<T = undefined>(
+	callback: UseEffectsCallback<T>,
+	deps: DependencyList | 'always',
+	options?: UseEffectsOptions,
+): UseEffectsReturn<T>
+
+function useEffects<T = undefined>(...args: any[]): UseEffectsReturn<T> {
+	const [callback, propsDeps, options] = parseArgs<T>(args)
+
+	const {
 		moment = 'effect',
 		useDigestProps = true,
-	}: UseEffectsOptions = {}
-): UseEffectsReturn<T> {
-	if (useDigestProps) {
-		// Pack deps into one predictible number
-		deps = [digestProps(deps)]
-	}
+	} = options
+
+	const deps = useDigestProps
+		? [digestProps(propsDeps)]
+		: propsDeps
 
 	const ref = useRef<T>(null) as MutableRefObject<T>
 	const destroyables = useMemo((): Destroyable[] => [], [])
@@ -70,10 +93,15 @@ export function useEffects<T = undefined>(
 	return { ref }
 }
 
-export function useLayoutEffects<T = undefined>(
+function useLayoutEffects<T = undefined>(
 	callback: (value: T) => Generator<void | Destroyable, void, unknown>,
 	deps: DependencyList | 'always',
 	options: Omit<UseEffectsOptions, 'moment'>
 ): UseEffectsReturn<T> {
 	return useEffects(callback, deps, { ...options, moment: 'layoutEffect' })
+}
+
+export {
+	useEffects,
+	useLayoutEffects,
 }
