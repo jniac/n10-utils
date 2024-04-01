e7d6b4f73420f6ae4a0be24835f32a2ac28f420a
jniac <joseph.merdrignac@gmail.com>
2024-04-02 00:45:32 +0200

    object/withoutUndefined + handleKeyboard w. target

--- dom/handle/keyboard.ts
+++ dom/handle/keyboard.ts
@@ -1,28 +1,52 @@
-import { Destroyable, StringFilter } from '../../types'
 import { applyStringFilter } from '../../string'
+import { Destroyable, StringFilter } from '../../types'
 
 type Info = {
   event: KeyboardEvent
 }
 
+const defaultOptions = {
+  preventDefault: false,
+}
+
+type Options = Partial<typeof defaultOptions>
+
 type KeyboardListenerEntry = [
-  filter: StringFilter, 
+  filter: StringFilter,
   callback: (info: Info) => void,
 ]
 
-export function handleKeyboard(listeners: KeyboardListenerEntry[]): Destroyable {
+function solveArgs(args: any[]): [HTMLElement, Options, KeyboardListenerEntry[]] {
+  if (args.length === 1) {
+    return [document.documentElement, {}, args[0]]
+  }
+  if (args.length === 2) {
+    return [args[0], {}, args[1]]
+  }
+  return args as any
+}
+
+export function handleKeyboard(listeners: KeyboardListenerEntry[]): Destroyable
+export function handleKeyboard(target: HTMLElement, listeners: KeyboardListenerEntry[]): Destroyable
+export function handleKeyboard(target: HTMLElement, options: Options, listeners: KeyboardListenerEntry[]): Destroyable
+export function handleKeyboard(...args: any[]): Destroyable {
+  const [target, options, listeners] = solveArgs(args)
+  const { preventDefault } = { ...defaultOptions, ...options }
   const onKeyDown = (event: KeyboardEvent): void => {
     const info: Info = { event }
     for (let i = 0, max = listeners.length; i < max; i++) {
       const [filter, callback] = listeners[i]
       if (applyStringFilter(event.code, filter)) {
+        if (preventDefault) {
+          event.preventDefault()
+        }
         callback(info)
       }
     }
   }
-  window.addEventListener('keydown', onKeyDown)
+  target.addEventListener('keydown', onKeyDown, { passive: false })
   const destroy = () => {
-    window.removeEventListener('keydown', onKeyDown)
+    target.removeEventListener('keydown', onKeyDown)
   }
   return { destroy }
 }
