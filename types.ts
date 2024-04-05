32fc1b78c467e5b08f69323686e246086195fa17
jniac <joseph.merdrignac@gmail.com>
2024-04-05 08:38:54 +0200

    OneOrMany

--- types.ts
+++ types.ts
@@ -1,4 +1,6 @@
-export type DestroyableObject = { destroy: () => void; }
-export type Destroyable = DestroyableObject | (() => void);
+export type DestroyableObject = { destroy: () => void }
+export type Destroyable = DestroyableObject | (() => void)
 
 export type StringFilter = '*' | string | RegExp | ((str: string) => boolean)
+
+export type OneOrMany<T> = T | T[]
