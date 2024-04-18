061b7fc756efbce8fed2ea32d259840e27be4118
jniac <joseph.merdrignac@gmail.com>
2024-04-18 22:04:43 +0200

    bendX / bendY + fix

--- packages/three/experimental/bend-modifier.ts
+++ packages/three/experimental/bend-modifier.ts
@@ -39,10 +39,36 @@ export function getBendModifier(matrix: Matrix4) {
       return out.applyMatrix4(_m)
     },
 
+    applyBendX: (radius: number, point: Vector3, out = point): Vector3 => {
+      if (radius > 1e12) {
+        return out.copy(point)
+      }
+      const { x, y, z } = _local.copy(point).applyMatrix4(_im)
+      const a = x / (radius - z)
+      const ox = (radius - z) * Math.sin(a)
+      const oz = radius - (radius - z) * Math.cos(a)
+      return out
+        .set(ox, y, oz)
+        .applyMatrix4(_m)
+    },
+
+    applyBendY: (radius: number, point: Vector3, out = point): Vector3 => {
+      if (radius > 1e12) {
+        return out.copy(point)
+      }
+      const { x, y, z } = _local.copy(point).applyMatrix4(_im)
+      const a = y / (radius - z)
+      const oy = (radius - z) * Math.sin(a)
+      const oz = radius - (radius - z) * Math.cos(a)
+      return out
+        .set(x, oy, oz)
+        .applyMatrix4(_m)
+    },
+
     applyCurvatureApprox: (radiusX: number, radiusY: number, point: Vector3, out = point): Vector3 => {
       const { x, y, z } = _local.copy(point).applyMatrix4(_im)
-      const ax = x / (radiusX + z)
-      const ay = y / (radiusY + z)
+      const ax = x / (radiusX - z)
+      const ay = y / (radiusY - z)
       const ox = radiusX < 1e12 ? (radiusX - z) * Math.sin(ax) : x
       const oy = radiusY < 1e12 ? (radiusY - z) * Math.sin(ay) : y
       const ozX = radiusX < 1e12 ? radiusX - (radiusX - z / 2) * Math.cos(ax) : z / 2
