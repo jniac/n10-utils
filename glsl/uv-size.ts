8de1b96872bed6d993cc8aa42d65c87f21249f04
jniac <joseph.merdrignac@gmail.com>
2024-04-20 12:53:17 +0200

    uv-size, for easier billboard uv manipulation

---
+++ glsl/uv-size.ts
@@ -0,0 +1,15 @@
+export const glsl_uv_size = /* glsl */ `
+  // sizeMode: 0 for "contain", 1 for "cover"
+  vec2 applyUvSize(vec2 uv, float outerAspect, float innerAspect, float sizeMode, vec2 align, vec2 scale) {
+    align.y = 1.0 - align.y; // Flip y-coordinate
+    if ((outerAspect > innerAspect) != (sizeMode == 1.0)) { // XOR condition for mode
+      float scaleFactor = outerAspect / innerAspect;
+      uv.x = (uv.x - align.x) * scaleFactor + align.x; // Adjust x-coordinate
+    } else {
+      float scaleFactor = innerAspect / outerAspect;
+      uv.y = (uv.y - align.y) * scaleFactor + align.y; // Adjust y-coordinate
+    }
+    uv = (uv - align) / scale + align;
+    return uv;
+  }
+`
