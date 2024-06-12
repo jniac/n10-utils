66a69df98eaa653cb048281ef42af5bb7b7e2ad0
jniac <joseph.merdrignac@gmail.com>
2024-06-13 01:03:28 +0200

    object/deep: deepWalk,deepGet, deepSet, deepDiff

--- object/deep.ts
+++ object/deep.ts
@@ -98,3 +98,128 @@ export function deepCopy<T extends object>(
 
   return hasChanged
 }
+
+type Path = (string | number | symbol)[]
+
+const deepWalkOptions = {
+  path: <Path | null>null,
+  ascendants: <any[] | null>null,
+  dateAsValue: true,
+  withConstructorAsValue: true,
+  onValue: <((value: any, path: Path, ascendants: any[]) => void) | null>null,
+  onObject: <((value: any, path: Path, ascendants: any[]) => void) | null>null,
+}
+/**
+ * Walks through the target object deeply and invokes the specified callbacks.
+ */
+export function deepWalk(target: any, options: Partial<typeof deepWalkOptions> = {}) {
+  const {
+    path = [],
+    ascendants = [],
+    dateAsValue: dateAsValue = deepWalkOptions.dateAsValue,
+    withConstructorAsValue = deepWalkOptions.withConstructorAsValue,
+  } = options
+  if (dateAsValue && target instanceof Date) {
+    options.onValue?.(target, path, ascendants)
+  }
+  if (withConstructorAsValue && target.constructor !== Object && target.constructor !== Array) {
+    options.onValue?.(target, path, ascendants)
+  }
+  else if (isObject(target)) {
+    options.onObject?.(target, path, ascendants)
+    for (const key in target) {
+      deepWalk(target[key], {
+        ...options,
+        path: [...path, key],
+        ascendants: [...ascendants, target],
+      })
+    }
+  }
+  else {
+    options.onValue?.(target, path, ascendants)
+  }
+}
+
+/**
+ * Deeply gets a value from the target object at the specified path.
+ */
+export function deepGet(target: any, path: Path | string): { value: any, exists: boolean } {
+  if (typeof path === 'string') {
+    path = path.split('.')
+  }
+  let value = target
+  for (const key of path) {
+    if (key in value) {
+      value = value[key]
+    }
+    else {
+      return { value: undefined, exists: false }
+    }
+  }
+  return { value, exists: true }
+}
+
+const deepSetOptions = {
+  ascendantsModel: <any[] | null>null,
+  createAscendants: true,
+}
+/**
+ * Deeply sets a value in the target object at the specified path.
+ */
+export function deepSet(target: any, path: Path | string, value: any, options: Partial<typeof deepSetOptions> = {}): { success: boolean, createdAscendants: boolean } {
+  if (typeof path === 'string') {
+    path = path.split('.')
+  }
+  const {
+    ascendantsModel,
+    createAscendants = deepSetOptions.createAscendants,
+  } = options
+  let scope = target
+  let createdAscendants = false
+  for (let index = 0, max = path.length - 1; index < max; index++) {
+    const key = path[index]
+    if (key in scope === false) {
+      if (createAscendants) {
+        const source = scope[key]
+        if (source === undefined) {
+          const ascendant = ascendantsModel?.[index] ?? (typeof path[index] === 'number' ? [] : {})
+          scope[key] = ascendant
+          scope = ascendant
+          createdAscendants = true
+        }
+      }
+      else {
+        return { success: false, createdAscendants }
+      }
+    }
+  }
+  scope[path[path.length - 1]] = value
+  return { success: true, createdAscendants }
+}
+
+/**
+ * Compares two objects deeply and returns the differences in a diff object.
+ */
+export function deepDiff(objectA: any, objectB: any) {
+  const diff = {
+    a: {},
+    b: {},
+  }
+  deepWalk(objectA, {
+    onValue(value, path) {
+      const { value: valueB, exists } = deepGet(objectB, path)
+      if (exists === false || value !== valueB) {
+        deepSet(diff.a, path, value, { createAscendants: true })
+      }
+    },
+  })
+  deepWalk(objectB, {
+    onValue(value, path) {
+      const { value: valueA, exists } = deepGet(objectA, path)
+      if (exists === false || value !== valueA) {
+        deepSet(diff.b, path, value, { createAscendants: true })
+      }
+    },
+  })
+  return diff
+}
