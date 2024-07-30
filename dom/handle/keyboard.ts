60b056c85834fe9ef9a2c041aebebab6b4cbed44
jniac <joseph.merdrignac@gmail.com>
2024-07-30 12:38:51 +0200

    keyboard "modifiers": support for callback + info

--- dom/handle/keyboard.ts
+++ dom/handle/keyboard.ts
@@ -3,6 +3,12 @@ import { Destroyable, StringMatcher } from '../../types'
 
 type Info = {
   event: KeyboardEvent
+  modifiers: {
+    ctrl: boolean
+    alt: boolean
+    shift: boolean
+    meta: boolean
+  }
 }
 
 const defaultOptions = {
@@ -16,7 +22,19 @@ type Combination<T extends string, U extends string = T> =
   T extends any
   ? T | `${T}+${Combination<Exclude<U, T>>}`
   : never
-type Modifiers = '' | Combination<Modifier>
+type Modifiers =
+  | ''
+  | Combination<Modifier>
+  | ((modifiers: { ctrl: boolean, alt: boolean, shift: boolean, meta: boolean }) => boolean)
+
+function modifiersMatch(event: KeyboardEvent, modifiers: Modifiers): boolean {
+  const { ctrlKey, altKey, shiftKey, metaKey } = event
+  if (typeof modifiers === 'function') {
+    return modifiers({ ctrl: ctrlKey, alt: altKey, shift: shiftKey, meta: metaKey })
+  }
+  const { ctrl = false, alt = false, shift = false, meta = false } = Object.fromEntries(modifiers.split('-').map(modifier => [modifier, true]))
+  return ctrl === ctrlKey && alt === altKey && shift === shiftKey && meta === metaKey
+}
 
 const defaultKeyboardFilter = {
   key: '*' as StringMatcher,
@@ -31,7 +49,6 @@ type KeyboardFilter = typeof defaultKeyboardFilter
 type KeyboardFilterDeclaration =
   | StringMatcher
   | Partial<KeyboardFilter>
-
 
 function solveKeyboardFilter(filter: KeyboardFilterDeclaration): KeyboardFilter {
   const result: KeyboardFilter = typeof filter === 'string'
@@ -58,6 +75,36 @@ function solveArgs(args: any[]): [HTMLElement, Options, KeyboardListenerEntry[]]
   return args as any
 }
 
+/**
+ * Usage:
+ * ```
+ * handleKeyboard([
+ *   // Ctrl + Z
+ *   [{ key: 'z', modifiers: m => !m.shift && (m.ctrl || m.meta) }, info => {
+ *     info.event.preventDefault()
+ *     history.undo()
+ *   }],
+ *   // Ctrl + Shift + Z
+ *   [{ key: 'z', modifiers: m => m.shift && (m.ctrl || m.meta) }, info => {
+ *     info.event.preventDefault()
+ *     history.redo()
+ *   }],
+ * ])
+ * ```
+ * Or:
+ * ```
+ * handleKeyboard([
+ *   [{ key: 'z', modifiers: m => m.ctrl || m.meta }, info => {
+ *     info.event.preventDefault()
+ *     if (info.modifiers.shift) {
+ *       editor.history.redo()
+ *     } else {
+ *       editor.history.undo()
+ *     }
+ *   }],
+ * ])
+ * ```
+ */
 export function handleKeyboard(listeners: KeyboardListenerEntry[]): Destroyable
 export function handleKeyboard(target: HTMLElement, listeners: KeyboardListenerEntry[]): Destroyable
 export function handleKeyboard(target: HTMLElement, options: Options, listeners: KeyboardListenerEntry[]): Destroyable
@@ -65,19 +112,21 @@ export function handleKeyboard(...args: any[]): Destroyable {
   const [target, options, listeners] = solveArgs(args)
   const { preventDefault } = { ...defaultOptions, ...options }
   const onKeyDown = (event: KeyboardEvent): void => {
-    const info: Info = { event }
+    const { ctrlKey, altKey, shiftKey, metaKey } = event
+    const info: Info = {
+      event,
+      modifiers: { ctrl: ctrlKey, alt: altKey, shift: shiftKey, meta: metaKey },
+    }
     for (let i = 0, max = listeners.length; i < max; i++) {
       const [filter, callback] = listeners[i]
       const { key, keyCaseInsensitive, code, noModifiers, modifiers } = solveKeyboardFilter(filter)
-      const { ctrl = false, alt = false, shift = false, meta = false } = Object.fromEntries(modifiers.split('-').map(modifier => [modifier, true]))
-      const { ctrlKey, altKey, shiftKey, metaKey } = event
 
       const eventKey = keyCaseInsensitive ? event.key.toLowerCase() : event.key
       const matches = {
         key: applyStringMatcher(eventKey, key),
         code: applyStringMatcher(event.code, code),
         noModifiers: !noModifiers || (ctrlKey === false && altKey === false && shiftKey === false && metaKey === false),
-        modifiers: !modifiers || (ctrl === ctrlKey && alt === altKey && shift === shiftKey && meta === metaKey),
+        modifiers: !modifiers || modifiersMatch(event, modifiers),
       }
 
       const match = Object.values(matches).every(Boolean)
